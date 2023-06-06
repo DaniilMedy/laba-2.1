8fa13ba4f08f67c780a7b565878c111077864a62
DaniilMedy <135615674+DaniilMedy@users.noreply.github.com>
2023-06-06 17:21:50 +0300

    Create individual.py

---
+++ individual.py
@@ -0,0 +1,5 @@
+celsius = float(input("Введите температуру по шкале Цельсия: "))
+fahrenheit = celsius * 1.8 + 32
+print("Температура по шкале Фаренгейта: ", fahrenheit)
+kelvin = celsius + 273.15
+print("Температура по шкале Кельвина: ", kelvin)
